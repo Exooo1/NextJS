a3a9f25d70897b5aa79849473cca263cbb82f11c
Maskalenchik Vlas <vlasmaskalenchik1998@gmail.com>
2022-11-30 21:27:46 +0300

    My adeventures in NextJS!

---
+++ pages/Button/SimpleButton.tsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+export const SimpleButton = () => {
+    const arrayColor: Array<string> = ['blue', 'orange', 'red', 'yellow', 'green', 'pink','#000043','#85f607','#8f049f','#12eab0']
+    const [color, setColor] = useState<number>(0)
+    const handlerColor = () => {
+        setColor(+(Math.random() * arrayColor.length).toFixed())
+    }
+    return <div>
+        <button onClick={handlerColor} style={{color: arrayColor[color], width: '100px', height: '60px'}}>color</button>
+    </div>
+}
